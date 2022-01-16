c12f3e2709985e4d7259a503df8dcef3fb06ef54
Felipa Mendez <felipalouise@gmail.com>
2022-01-15 20:43:02 -0600

    commit tree 9b8bf1330819c2b8adbe9e2ccb79a710067d950c

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ const newHeader = document.createElement("h1");
 //document.body.append(newHeader);
 newHeader.className = "victory";
 newHeader.id = "victory";
-newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.textContent = "Felipa is the champion";
+
